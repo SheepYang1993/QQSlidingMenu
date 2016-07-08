51a74e26f33be375965d94805c1c97b414555898
SheepYang <332594623@qq.com>
2016-07-08 15:05:28 +0800

    添加DragLayout,重点学习ViewDragHelper; 添加了com.nineoldandroids:library:2.4.0依赖;

---
+++ app/src/main/java/com/sheepyang/qqsliddingmenu/test/TestActivity.java
@@ -0,0 +1,24 @@
+package com.sheepyang.qqsliddingmenu.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.sheepyang.qqsliddingmenu.R;
+
+
+public class TestActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test);
+	}
+//
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return true;
+//	}
+
+}
